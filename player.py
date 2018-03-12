66ad4b727c302e4e7ea131209cc28d23e20a664e
Michael Peng (RD-TW) <michael_peng@tw-michaelpeng.client.tw.trendnet.org>
2018-03-12 17:42:55 +0800

    update player to consider more variables

--- player.py
+++ player.py
@@ -4,7 +4,9 @@
 from agent_common import *
 
 server = sys.argv[1] if len(sys.argv)>1 else 'beta'
-if server == 'training':
+if server == 'battle':
+    url  = 'ws://allhands2018-battle.dev.spn.a1q7.net:3001'
+elif server == 'training':
     url  = 'ws://allhands2018-training.dev.spn.a1q7.net:3001'
 else:
     url  = 'ws://allhands2018-beta.dev.spn.a1q7.net:3001'
@@ -19,6 +21,7 @@ def agent_jyp(event,data):
         #
         #-- Calculate Basic Stats and Win Probability --#
         #
+        #-- Game State Variables --#
         input_var  = pd.Series()
         input_var['N']   = len(data['game']['players'])
         input_var['Nnf'] = len([x for x in data['game']['players'] if not x['folded']])
@@ -29,6 +32,7 @@ def agent_jyp(event,data):
         input_var['hole']  = cards_to_str(hole)
         input_var['board'] = cards_to_str(board)
         #
+        #-- Win Probability --#
         prWin_OK  = False
         if input_var.roundName == 'deal' and input_var.N <= 10:
             try:
@@ -51,19 +55,29 @@ def agent_jyp(event,data):
                 input_var['prWin'],input_var['prWinStd'] = calculate_win_prob(input_var.N,hole,Nsamp=input_var['Nsim'])
             prWin_OK  = True
         #
-        input_var['pot']    = sum([x['roundBet'] for x in data['game']['players']])
-        input_var['bet']    = data['self']['bet']
-        input_var['maxBet'] = max([x['bet'] for x in data['game']['players']])
-        input_var['NMaxBet'] = len([x for x in data['game']['players'] if x['bet']==input_var.maxBet])
+        #-- Betting Variables --#
+        input_var['chips']  = data['self']['chips']
+        input_var['reld']   = data['self']['reloadCount']
+        input_var['pot']    = data['self']['roundBet'] # self accumulated contribution to pot
+        input_var['bet']    = data['self']['bet'] # self bet on this round
         input_var['minBet'] = data['self']['minBet']
+        input_var['sumPot_all'] = sum([x['roundBet'] for x in data['game']['players']])
+        input_var['sumBet_all'] = sum([x['bet'] for x in data['game']['players']])
+        input_var['maxBet_all'] = max([x['bet'] for x in data['game']['players']])
+        input_var['N_maxBet_all']  = len([x for x in data['game']['players'] if x['bet']==input_var.maxBet_all])
+        for x in data['game']['players']:
+            x['minBet'] = input_var.maxBet_all - x['bet']
+        input_var['N_canraise']    = len([x for x in data['game']['players'] if x['chips'] > x['minBet'] and not x['folded']])
+        input_var['sumMinBet_all'] = len([min(x['minBet'],x['chips']) for x in data['game']['players'] if x['bet']<input_var.maxBet_all and not x['folded']])
         #
-        input_var['util_fold']  = -data['self']['roundBet'] - data['self']['bet']
-        input_var['util_call']  = input_var.prWin*input_var.pot + input_var.prWin*input_var.maxBet*input_var.Nnf - data['self']['roundBet'] - input_var.maxBet
-        input_var['util_raise_coeff']  = input_var.prWin*input_var.Nnf - 1
+        #-- Decision Support Variables --#
+        input_var['util_fold']  = -input_var.pot - input_var.bet
+        input_var['util_call']  = input_var.prWin*(input_var.sumPot_all + input_var.sumBet_all + input_var.sumMinBet_all) - input_var.pot - input_var.bet - min(input_var.minBet,input_var.chips)
+        input_var['util_raise_coeff']  = input_var.prWin*input_var.N_canraise - 1
         #
-        # Worst case scenario utility (everyone but one folds, i.e. a dual)
-        input_var['util_call2'] = input_var.prWin*input_var.pot + input_var.prWin*input_var.maxBet*2 - data['self']['roundBet'] - input_var.maxBet
-        input_var['util_raise_coeff2'] = input_var.prWin*2 - 1
+        #-- Worst case scenario utility (everyone but one folds, i.e. a dual) --#
+        input_var['util_call2']  = input_var.prWin*(input_var.sumPot_all + input_var.sumBet_all) - input_var.pot - input_var.bet - min(input_var.minBet,input_var.chips)
+        input_var['util_raise_coeff2']  = input_var.prWin*2 - 1
         #
         if input_var.minBet > 0:
             # Need to "pay" to stay in game
