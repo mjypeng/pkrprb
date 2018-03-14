06594ccc60292c99334cc9031821dfc81d102961
彭智楹 <pencilo@gmail.com>
2018-03-15 06:38:26 +0800

    ...

--- pkr_sim.py
+++ pkr_sim.py
@@ -6,3 +6,8 @@ for filename in filelist:
 
 
 list(zip(*filelist[0].rsplit(']')[0].rsplit('[')[1].split()))
+
+
+# suit eq:  4 x (13 2)
+# order eq: (4 2) x 13
+# suit & order neq: (4 2) x (13 2) x 2
