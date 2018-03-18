4b61d2375eef0e933133108acc0b308d71f67a99
彭智楹 <pencilo@gmail.com>
2018-03-18 22:00:13 +0800

    ...

--- pkr_sim.py
+++ pkr_sim.py
@@ -1,28 +1,25 @@
 from common import *
 import os,glob
 
-results   = pd.DataFrame()
-deck  = new_deck()
-deck  = deck[deck.s.isin(('♥','♠'))]
-for idx1,c1 in deck.iterrows():
-    for idx2,c2 in deck.iterrows():
-        if not c1.equals(c2) and c1.s == '♠' and c1.o >= c2.o:
-            hole  = pd.concat([c1,c2],1).transpose()
-            idx   = cards_to_str(hole)
-            results.loc[idx,'suit']  = int(c1.s==c2.s)
-            results.loc[idx,'rank']  = int(c1.o==c2.o)
-            results.loc[idx,'order'] = int(np.abs(c1.o-c2.o)==1)
-            results.loc[idx,'prob']  = (4 if c1.s==c2.s else (6 if c1.o==c2.o else 12))/(26*51)
-            for N in range(2,11):
-                results.loc[cards_to_str(hole),N] = read_win_prob(N,hole)[0]
-            print(results.loc[idx],'\n')
-
-results.index.names = ('hole',)
-results.to_csv('deal_win_prob.csv',encoding='utf-8-sig')
-
-
-
-
+# #-- Generate deal win prob table --#
+# results   = pd.DataFrame()
+# deck  = new_deck()
+# deck  = deck[deck.s.isin(('♥','♠'))]
+# for idx1,c1 in deck.iterrows():
+#     for idx2,c2 in deck.iterrows():
+#         if not c1.equals(c2) and c1.s == '♠' and c1.o >= c2.o:
+#             hole  = pd.concat([c1,c2],1).transpose()
+#             idx   = cards_to_str(hole)
+#             results.loc[idx,'suit']  = int(c1.s==c2.s)
+#             results.loc[idx,'rank']  = int(c1.o==c2.o)
+#             results.loc[idx,'order'] = int(np.abs(c1.o-c2.o)==1)
+#             results.loc[idx,'prob']  = (4 if c1.s==c2.s else (6 if c1.o==c2.o else 12))/(26*51)
+#             for N in range(2,11):
+#                 results.loc[cards_to_str(hole),N] = read_win_prob(N,hole)[0]
+#             print(results.loc[idx],'\n')
+#
+# results.index.names = ('hole',)
+# results.to_csv('deal_win_prob.csv',encoding='utf-8-sig')
 
 results   = pd.DataFrame(index=pd.MultiIndex(levels=[[],[]],labels=[[],[]]))
 filelist  = glob.glob('sim_prob' + os.sep + 'sim2_*')
--- player3.py
+++ player3.py
@@ -20,16 +20,20 @@ m.update(name.encode('utf8'))
 name_md5 = m.hexdigest()
 
 # Agent State
-GAMBLE_STATE  = True
-GAMBLE_STATE_TRANSITION = 0.15
-BANKRUPT_TOL  = 0.1 # Taking the same prWin chance, make sure we only risk a limited amount each trial so there's (1-BANKRUPT_TOL)% chance we win one trial before bankruptcy
-GAMBLE_THD    = 0.1 # How much percentage of total asset to gamble when the odds (utility to call/bet/raise) are against us
+SMALL_BLIND  = 0
+NUM_BLIND    = 0
+# GAMBLE_STATE  = True
+# GAMBLE_STATE_TRANSITION = 0.15
+BANKRUPT_TOL  = 0.05 # Taking the same prWin chance, make sure we only risk a limited amount each trial so there's (1-BANKRUPT_TOL)% chance we win one trial before bankruptcy
+# GAMBLE_THD    = 0.1 # How much percentage of total asset to gamble when the odds (utility to call/bet/raise) are against us
 
 def agent_jyp(event,data):
-    global GAMBLE_STATE
-    global GAMBLE_STATE_TRANSITION
+    global SMALL_BLIND
+    global NUM_BLIND
+    # global GAMBLE_STATE
+    # global GAMBLE_STATE_TRANSITION
     global BANKRUPT_TOL
-    global GAMBLE_THD
+    # global GAMBLE_THD
     #
     if event in ('__action','__bet'):
         #
@@ -86,70 +90,43 @@ def agent_jyp(event,data):
         players['cost_on_table'] = players.roundBet + players.bet
         players['cost_to_call']  = np.minimum(state.maxBet_all - players.bet,players.chips)
         #
-        util_call_lose = -np.minimum(players.cost_on_table + players.cost_to_call,state.cost_total).mean()
-        util_call_win  = state.cost_total + np.minimum(players.cost_on_table + players.cost_to_call,state.cost_total).sum()
-        state['util_fold']   = -state.cost_on_table
-        state['util_call']   = state.prWin*util_call_win + (1-state.prWin)*util_call_lose
-        state['N_canraise']  = (~players.folded & (players.chips>players.cost_to_call)).sum()
-        state['util_raise_coeff'] = state.prWin*(state.N_canraise + 1) - 1
-        #
-        #-- Worst case scenario utility (everyone afterwards folds, becomes a dual in the worst case) --#
-        util_call_win2 = state.cost_total + np.minimum(players.cost_on_table,state.cost_total).sum()
-        state['util_call2']        = state.prWin*util_call_win2 + (1-state.prWin)*util_call_lose
-        state['util_raise_coeff2'] = state.prWin*2 - 1
+        state['util_call']        = state.prWin*(players.cost_on_table.sum() + state.cost_on_table + 2*state.cost_to_call) - state.cost_to_call
+        state['util_raise_coeff'] = state.prWin*2 - 1
         #
         #-- Betting limit heuristic --#
-        num_round_to_bankrupt  = min(np.log(BANKRUPT_TOL)/np.log(1-state.prWin),state.chips_total*5/(data['game']['bigBlind']['amount']+data['game']['smallBlind']['amount']))
-        state['limitRoundBet'] = state.chips_total/num_round_to_bankrupt if state.prWin<1 else state.chips_total
-        state['limitBet']      = state.limitRoundBet - state.cost_on_table
+        A  = state.chips_total
+        if A <= 2*(state.N - NUM_BLIND)*SMALL_BLIND:
+            T_BB  = np.ceil(A/(2*SMALL_BLIND))
+        else:
+            A    -= 2*(state.N - NUM_BLIND)*SMALL_BLIND
+            T_BB  = state.N - NUM_BLIND
+            k     = 4
+            while A > 0:
+                if A > k*state.N*SMALL_BLIND:
+                    A    -= k*state.N*SMALL_BLIND
+                    T_BB += state.N
+                    k    *= 2
+                else:
+                    T_BB += np.ceil(A/(k*SMALL_BLIND))
+                    A     = 0
+        #
+        T_Bet  = min(np.log(BANKRUPT_TOL)/np.log(1-state.prWin),T_BB)
+        state['limitBet'] = state.chips_total/T_Bet if state.prWin<1 else state.chips_total
         #
         #-- Decision Logic --#
-        base_prWin  = 1/state.N
-        allin_prWin = 0.95
-        bet_mult    = 0.9 #0.1
-        # if state.roundName == 'deal':
-        #     bet_mult  = 0.2
-        # elif state.roundName == 'flop':
-        #     bet_mult  = 0.5
-        # elif state.roundName == 'turn':
-        #     bet_mult  = 0.618
-        # elif state.roundName == 'river':
-        #     bet_mult  = 0.8
-        state['gamble']      = GAMBLE_STATE
-        state['gamble_tol']  = max(int(state.chips_total*GAMBLE_THD),2*data['game']['bigBlind']['amount'])
         #
         if state.cost_to_call > 0:
             # Need to pay "cost_to_call" to stay in game
-            if state.Nsim > 200 and state.prWin > allin_prWin:
-                resp = ('allin',0)
-            elif state.Nsim > 200 and state.prWin < base_prWin and state.roundName != 'deal':
+            if state.util_call < 0:
                 resp = ('fold',0)
-            elif state.util_fold > state.util_call:
-                if GAMBLE_STATE and state.util_fold < 0 and state.util_fold - state.util_call <= state.gamble_tol:
-                    resp = ('call',0)
-                else:
-                    resp = ('fold',0)
-            elif state.cost_to_call > state.limitBet:
-                if GAMBLE_STATE and state.cost_to_call <= state.gamble_tol:
-                    resp = ('call',0)
-                else:
-                    resp = ('fold',0)
             elif state.util_raise_coeff > 0:
-                if GAMBLE_STATE or state.util_raise_coeff > 0.2:
-                    resp = ('bet',int(state.limitBet*bet_mult))
-                else:
-                    resp = ('call',0)
+                resp = ('bet',int(state.limitBet))
             else:
                 resp = ('call',0)
         else:
             # Can stay in the game for free
-            if state.Nsim > 200 and state.prWin > allin_prWin:
-                resp = ('allin',0)
-            elif state.util_raise_coeff > 0:
-                if GAMBLE_STATE or state.util_raise_coeff > 0.3:
-                    resp = ('bet',int(state.limitBet*bet_mult))
-                else:
-                    resp = ('check',0)
+            if state.util_raise_coeff > 0:
+                resp = ('bet',int(state.limitBet))
             else:
                 resp = ('check',0)
         #
@@ -166,8 +143,14 @@ def agent_jyp(event,data):
                 break
         calculate_win_prob_mp_start(N,hole,board,n_jobs=3)
         #
-        if event == '__new_round' and random.random() < GAMBLE_STATE_TRANSITION:
-            GAMBLE_STATE  = not GAMBLE_STATE
+        # if event == '__new_round' and random.random() < GAMBLE_STATE_TRANSITION:
+        #     GAMBLE_STATE  = not GAMBLE_STATE
+        if event == '__new_round':
+            if SMALL_BLIND != data['table']['smallBlind']['amount']:
+                SMALL_BLIND  = data['table']['smallBlind']['amount']
+                NUM_BLIND    = 1
+            else:
+                NUM_BLIND   += 1
     elif event in ('__round_end','__game_over','__game_stop'):
         calculate_win_prob_mp_stop()
     elif event == '__start_reload':
