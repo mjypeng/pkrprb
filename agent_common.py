0af9e190ccc321fc4fdf3a45816528213ae78b70
Michael Peng (RD-TW) <michael_peng@tw-michaelpeng.local>
2018-03-24 14:18:55 +0800

    ...

--- agent_common.py
+++ agent_common.py
@@ -16,7 +16,7 @@ game_actions = None
 player_stats = None
 expsmo_alpha = 0.1
 
-playerNames = ['jyp','jyp0','jyp1','jyp2','jyp3','jyp4','jyp5','twice','Samuel','steven','465fc773c4','basic1','basic2','basic3','basic4','random1','random2','random3','pot','fold','cat1','cat4','cat5','8+9','87-dawn-ape','87-rising-ape','87945','V.S.A.','basic65536','houtou_a','houtou_p','tomas','lefthand_cat','89','basic','cat0','cat1','cat2','cat3','cat4','cat5','cat6','cat7','cat8','cat9','basic_a','basic_b','basic_c','tomas2','fold1','pot1']
+playerNames = ['jyp','jyp0','jyp1','jyp2','jyp3','jyp4','jyp5','twice','Samuel','steven','465fc773c4','basic1','basic2','basic3','basic4','random1','random2','random3','pot','fold','cat1','cat4','cat5','8+9','87-dawn-ape','87-rising-ape','87945','V.S.A.','basic65536','houtou_a','houtou_p','tomas','lefthand_cat','89','basic','cat0','cat1','cat2','cat3','cat4','cat5','cat6','cat7','cat8','cat9','basic_a','basic_b','basic_c','tomas2','fold1','pot1','random_a','random_b']
 # cat4 => Leo
 # cat9 => Teebone
 # cat1 => jyp
